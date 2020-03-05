69eb0fd2fb1c0bd1e24af48aaf03b05979935f92
P2411795 <P2411795@my365.dmu.ac.uk>
2020-03-05 16:22:07 +0000

    Changed port to allow email from email

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 	MAIL_SERVER = 'smtp.gmail.com'
-	MAIL_PORT = 465 #465 587
+	MAIL_PORT = 587 #465 587
 	MAIL_USE_TLS = True
 	MAIL_USE_SSL = False
 	MAIL_USERNAME = 'fakenameminecraft@gmail.com'
--- migrations/versions/abc3d6c1b382_.py
+++ migrations/versions/abc3d6c1b382_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 4fef27f7e22b
+Revision ID: abc3d6c1b382
 Revises: 
-Create Date: 2020-03-01 14:54:39.257180
+Create Date: 2020-03-05 16:20:23.443611
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '4fef27f7e22b'
+revision = 'abc3d6c1b382'
 down_revision = None
 branch_labels = None
 depends_on = None
